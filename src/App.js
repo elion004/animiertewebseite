837ea2de0af5aeb196b06f129df4c9aebb550b24
elion <elionbajrami0404@gmail.com>
2023-04-03 14:58:09 +0200

    added first shoe

--- src/App.js
+++ src/App.js
@@ -18,12 +18,7 @@ function App() {
     return (
         <div style={{backgroundColor: "lightgoldenrodyellow"}}>
             <Container>
-                <div style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    height: '100vh',
-                }}>
+                <div className="vh-100 d-flex justify-content-center align-items-center">
                     <h1 style={{fontSize:'500%', fontStyle:'italic'}}>
                         <Typist typingDelay={100} cursor={<span className='cursor'>|</span>}>
                             Banana shoes are simple, boring and lame.
@@ -35,15 +30,28 @@ function App() {
                     </h1>
                 </div>
 
-                <div style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    height: '100vh',
-                }}>
-                    <h2>
-                        test
+                <div className="text-center vh-100">
+                    <h2 className="fw-normal display-2">
+                        Banana shoe 01
                     </h2>
+                    <br/>
+                    <div className="text-center align-middle">
+
+                        <Row>
+                            <Col style={{width: '200px'}}>
+                                <div data-aos="fade-up">
+                                    <img src={image1} alt={'shoe number one'} style={{height: '200px'}}/>
+                                </div>
+                            </Col>
+                            <Col>
+                                <p data-aos="fade-down" className="lead">
+                                    <LoremIpsum>
+                                    </LoremIpsum>
+                                </p>
+                            </Col>
+                        </Row>
+                    </div>
+
                 </div>
             </Container>
         </div>
