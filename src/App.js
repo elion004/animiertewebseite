a5f0147006162ea219f85d27f4746764dc03147d
Kovacevic Stevan <stevan.kovacevic@bbcag.ch>
2023-04-03 15:49:09 +0200

    built in gifs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import image1 from './images/dalle-e/DALL-E_Img1.png'
+import gif1 from './images/output-onlinegiftools.gif'
 import {useEffect} from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import {Card, Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.css'
-import LoremIpsum from "react-lorem-ipsum";
 import Typist from "react-typist-component";
 
 function App() {
@@ -25,14 +25,14 @@ function App() {
                             <Typist.Delay ms={1000}/>
                             <Typist.Backspace count={24}/>
                             <Typist.Delay ms={1000}/>
-                            <p style={{color:'red'}}>UNSURPASSABLE, STYLISH AND SIMPLY THE BEST!!!</p>
+                            <p style={{color: 'red'}}>UNSURPASSABLE, STYLISH AND SIMPLY THE BEST!!!</p>
                         </Typist>
                     </h1>
                 </div>
 
                 <div className="text-center vh-100">
-                    <h2>
-                        Banana shoe 01
+                    <h2 data-aos='fade-up' className="fw-normal display-2">
+                        BananaMax 97
                     </h2>
                     <br/>
                     <div className="text-center align-middle">
@@ -44,9 +44,14 @@ function App() {
                                 </div>
                             </Col>
                             <Col>
-                                <p data-aos="fade-down" className="lead text-start">
-                                    <LoremIpsum>
-                                    </LoremIpsum>
+                                <p data-aos="fade-up" className="lead">
+                                    The BananaMax 97 shoes are a unique and eye-catching footwear option for those who
+                                    want to make a statement. These shoes feature a bright yellow banana design on the
+                                    exterior, complete with black spots to mimic the fruit's natural appearance. In
+                                    addition, the shoes are infused with a sweet banana scent that will leave you
+                                    feeling energized and refreshed all day long. The BananaMax 97 shoes are perfect for
+                                    those who want to stand out in a crowd and add a touch of fun to their everyday
+                                    look.
                                 </p>
                             </Col>
                         </Row>
@@ -54,10 +59,11 @@ function App() {
 
                 </div>
             </Container>
+            <footer className={'fixed-bottom'}>
+                <img src={gif1} alt="a dancing banana" style={{maxWidth: '125px', maxHeight: '125px'}}/>
+            </footer>
         </div>
-
-    )
-        ;
+    );
 }
 
 export default App;
